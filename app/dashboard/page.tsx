b645bf138a9a2d4ca8dadaeeb366484c6be7a438
fawredd <moore.marcos@gmail.com>
2024-01-23 21:48:11 -0300

    [FEAT] <Links />

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
-import Sidenav from '@/app/ui/dashboard/sidenav';
 import { lusitana } from '@/app/ui/fonts';
 
 import {
@@ -23,7 +22,6 @@ export default async function Page() {
 
     return (
     <main>
-        <Sidenav />
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
         </h1>
